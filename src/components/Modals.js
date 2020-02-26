279dfcdb01bf0b17a21f4f57d337efb7bab09ad6
Hillel Arnold <helrond@hotmail.com>
2020-02-25 22:29:04 -0500

    use plain h2 instead of badges, fixes #9

--- src/components/Modals.js
+++ src/components/Modals.js
@@ -74,7 +74,7 @@ export class MapComponentModal extends Component {
             }
             { this.state.activeComponent.archivesspace_uri ? (
               <div className="mt-2">
-                <h2><Badge color="secondary">{this.state.activeComponent.title}</Badge></h2>
+                <h2>{this.state.activeComponent.title}</h2>
                 <p className="text-muted">{this.state.activeComponent.archivesspace_uri}</p>
                 <Button
                   color="warning"
