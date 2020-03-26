32e42db664958b454957cf21967d40fd4e75d45b
Hannah <hsistrunk@gmail.com>
2020-03-26 12:03:51 -0400

    include aria-disabled state for screenreaders

--- src/components/Modals.js
+++ src/components/Modals.js
@@ -100,6 +100,7 @@ export class MapComponentModal extends Component {
                   className="btn btn-sm btn-secondary"
                   onClick={() => this.fetchResource(this.state.resourceId)}
                   disabled={!this.state.resourceId}
+                  aria-disabled={!this.state.resourceId}
                   >
                   Fetch from ArchivesSpace
                 </Button>
