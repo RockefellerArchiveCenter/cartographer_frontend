c58adbfd8e0959fa9d2ccf723aec272f5b596f34
Hillel Arnold <helrond@hotmail.com>
2020-04-17 13:38:15 -0500

    change text on load

--- src/components/Modals.js
+++ src/components/Modals.js
@@ -21,6 +21,7 @@ export class MapComponentModal extends Component {
     this.toggle = this.props.toggle
     this.onSubmit = this.props.onSubmit
     this.state = {
+      archivesSpaceButtonText: "Fetch from ArchivesSpace",
       activeMap: this.props.activeMap,
       activeComponent: this.props.activeComponent,
       resourceId: "",
@@ -49,11 +50,16 @@ export class MapComponentModal extends Component {
     this.setState({ activeComponent: this.props.activeComponent })
   };
   fetchResource = resourceId => {
+    this.setState({archivesSpaceButtonText: "Fetching..."})
     this.setState({error: ""})
     axios
       .get(`/api/fetch-resource/${resourceId}`)
       .then(res => this.toggleData(res))
-      .catch(error => this.setState({error: error.response.data}));
+      .catch(error => this.setState({
+          error: error.response.data,
+          archivesSpaceButtonText: "Fetch from ArchivesSpace"
+      })
+    );
   };
   toggleTab = tab => {
     if(this.activeTab !== tab) this.setState({activeTab: tab});
@@ -101,7 +107,7 @@ export class MapComponentModal extends Component {
                     onClick={() => this.fetchResource(this.state.resourceId)}
                     disabled={!this.state.resourceId}
                   >
-                    Fetch from ArchivesSpace
+                    {this.state.archivesSpaceButtonText}
                   </Button>
                 </Form>
               </div>)}
